29ba28ad05dd28ce31c9a1da69b9001e650ae209
Xiao-Xian-Jie <t105360046@ntut.org.tw>
2019-05-01 21:28:06 +0800

    fix valid and test generator

--- HW2/src/t105360046_CNN_20190501_448.py
+++ HW2/src/t105360046_CNN_20190501_448.py
@@ -21,15 +21,12 @@ os.chdir(os.path.dirname(os.path.realpath(__file__)))
 
 output = True
 split_train = True
-save_to_dir = False
 
 verbose = 1
-batch_size = 2
+batch_size = 4
 epochs = 100
 image_width = 448
 validation_split = 0.1
-#color_mode='rgb'
-color_mode='grayscale'
 prediction_target_name = 'character'
 metrics = ['acc']
 loss = 'categorical_crossentropy'
@@ -41,10 +38,64 @@ tmp_dir = '../input/tmp/'
 gpu_options = tf.GPUOptions(allow_growth=True)
 sess = tf.Session(config=tf.ConfigProto(gpu_options=gpu_options, device_count={'GPU':1 , 'CPU':4}))
 keras.backend.set_session(sess)
+'''
+characters_20 = ['{}'.format(i) for i in os.listdir(train_dir)]
 
+t = time.time()
+try:
+    X_train = np.load(tmp_dir+str(image_width)+'_X_train.npy')
+    Y_train = np.load(tmp_dir+str(image_width)+'_Y_train.npy')
+except FileNotFoundError:
+    train_path = pd.DataFrame(np.array([]).reshape(0,2), columns=['image','character'], dtype='str')
+    for index,character in enumerate(characters_20):
+        paths = np.array(['../input/train/{}/{}'.format(character,i) for i in os.listdir(train_dir+character)]).T.reshape(-1,1)
+        
+        train_character = np.append(paths,(index)*np.ones(shape=paths.shape, dtype='uint8'),axis=1)
+        train_path = pd.DataFrame(np.append(train_path,train_character, axis=0), columns=['image','character'])
+    train_path = train_path.sample(frac=1).reset_index(drop=True)
+
+    print('loading images')
+    X_train = np.array(train_path.drop(columns=[prediction_target_name]).values, dtype='str').reshape(-1)
+    Y_train = np.array(train_path[prediction_target_name].values, dtype='uint8').reshape(-1)
+    X_train = [cv2.resize(cv2.imread(i, cv2.IMREAD_GRAYSCALE), (image_width,image_width), interpolation=cv2.INTER_CUBIC) 
+            for i in X_train.tolist()]
+    np.save(tmp_dir+str(image_width)+'_X_train.npy',X_train)
+    np.save(tmp_dir+str(image_width)+'_Y_train.npy',Y_train)
+    print('train created')
+
+if split_train:
+    X_train, X_valid, Y_train, Y_valid = train_test_split(X_train, Y_train, test_size=0.1, random_state=2)
+
+try:
+    X_test = np.load(tmp_dir+str(image_width)+'_X_test.npy')
+except FileNotFoundError:
+    X_test_path = ['../input/test/{}'.format(i) for i in os.listdir(test_dir)]
+    convert = lambda text: int(text) if text.isdigit() else text
+    alphanum_key = lambda key: [ convert(c) for c in re.split('([0-9]+)', key) ]
+    X_test_path.sort(key=alphanum_key)
+    
+    X_test = [cv2.resize(cv2.imread(i, cv2.IMREAD_COLOR), (image_width,image_width), interpolation=cv2.INTER_CUBIC) 
+                for i in X_test_path]
+    np.save(tmp_dir+str(image_width)+'_X_test.npy',X_test)
+
+elapsed = time.time() - t
+print(str(elapsed)+' s')
+Y_train = np.array(Y_train, copy=False).astype('uint8')
+#X_train /= 255
+Y_train = keras.utils.to_categorical(Y_train, len(characters_20))
+
+if split_train:
+    #X_valid = np.array(X_valid, copy=False).astype('float32')
+    Y_valid = np.array(Y_valid, copy=False).astype('uint8')
+    #X_valid /= 255
+    Y_valid = keras.utils.to_categorical(Y_valid, len(characters_20))
+
+
+X_test = np.array(X_test)
+'''
 #optimizer = optimizers.adadelta(lr=1.0, rho=0.95, epsilon=1e-24, decay=0.0)
 #optimizer = optimizers.adam(lr=0.001, beta_1=0.9, beta_2=0.999, epsilon=1e-24, decay=0, amsgrad=False)
-optimizer = optimizers.rmsprop(lr=0.001)
+optimizer = optimizers.rmsprop(lr=0.0001)
 #optimizer = optimizers.sgd(lr=0.01, momentum=0, decay=0, nesterov=False)
 
 #kernel_initializer = initializers.normal(mean=0, stddev=0.5, seed=None)
@@ -53,7 +104,7 @@ optimizer = optimizers.rmsprop(lr=0.001)
 #kernel_initializer = initializers.orthogonal(gain=1, seed=None)
 #kernel_initializer = initializers.identity(gain=1)
 #kernel_initializer = initializers.he_uniform()
-kernel_initializer = initializers.glorot_uniform()
+#kernel_initializer = initializers.glorot_uniform()
 
 class RestoreBestWeightsFinal(keras.callbacks.Callback):
     def __init__(self,
@@ -105,25 +156,80 @@ callbacks.append(RestoreBestWeightsFinal())
 #callbacks = None
 
 model = Sequential()
-#'''224
+#'''448
 bbbb = 32
-model.add(Conv2D(bbbb*1, (3, 3), activation='relu', input_shape=(image_width,image_width,1 if color_mode=='grayscale' else 3)))
-model.add(MaxPooling2D((2, 2)))
-model.add(Conv2D(bbbb*1, (3, 3), activation='relu'))
-model.add(MaxPooling2D((2, 2)))
-model.add(Conv2D(bbbb*2, (1, 1), activation='relu'))
-model.add(MaxPooling2D((2, 2)))
-model.add(Conv2D(bbbb*2, (1, 1), activation='relu'))
-model.add(MaxPooling2D((2, 2)))
-model.add(Conv2D(bbbb*4, (1, 1), activation='relu'))
-model.add(MaxPooling2D((2, 2)))
-model.add(Conv2D(bbbb*4, (1, 1), activation='relu'))
-model.add(MaxPooling2D((2, 2)))
-model.add(Conv2D(bbbb*4, (1, 1), activation='relu'))
+model.add(Conv2D(int(bbbb*1.5), (3, 3), padding='same', activation='relu', strides=(2, 2), input_shape=(image_width,image_width,3)))
+model.add(MaxPooling2D((2, 2), strides=(2, 2)))
+model.add(Conv2D(bbbb*2, (3, 3), padding='same', activation='relu'))
+model.add(MaxPooling2D((2, 2), strides=(2, 2)))
+model.add(Conv2D(bbbb*1, (1, 1), padding='same', activation='relu'))
+model.add(Conv2D(bbbb*2, (3, 3), padding='same', activation='relu'))
+model.add(Conv2D(bbbb*2, (1, 1), padding='same', activation='relu'))
+model.add(Conv2D(bbbb*4, (3, 3), padding='same', activation='relu'))
+model.add(MaxPooling2D((2, 2), strides=(2, 2)))
+model.add(Conv2D(bbbb*2, (1, 1), padding='same', activation='relu'))
+model.add(Conv2D(bbbb*4, (3, 3), padding='same', activation='relu'))
+model.add(Conv2D(bbbb*2, (1, 1), padding='same', activation='relu'))
+model.add(Conv2D(bbbb*4, (3, 3), padding='same', activation='relu'))
+model.add(Conv2D(bbbb*2, (1, 1), padding='same', activation='relu'))
+model.add(Conv2D(bbbb*4, (3, 3), padding='same', activation='relu'))
+model.add(Conv2D(bbbb*2, (1, 1), padding='same', activation='relu'))
+model.add(Conv2D(bbbb*4, (3, 3), padding='same', activation='relu'))
+model.add(Conv2D(bbbb*4, (1, 1), padding='same', activation='relu'))
+model.add(Conv2D(bbbb*8, (3, 3), padding='same', activation='relu'))
+model.add(MaxPooling2D((2, 2), strides=(2, 2)))
+model.add(Conv2D(bbbb*4, (1, 1), padding='same', activation='relu'))
+model.add(Conv2D(bbbb*8, (3, 3), padding='same', activation='relu'))
+model.add(Conv2D(bbbb*4, (1, 1), padding='same', activation='relu'))
+model.add(Conv2D(bbbb*8, (3, 3), padding='same', activation='relu'))
+model.add(Conv2D(bbbb*8, (3, 3), padding='same', activation='relu'))
+model.add(Conv2D(bbbb*8, (3, 3), padding='same', activation='relu', strides=(2, 2)))
 #'''
+'''256
+model.add(Conv2D(32, (3, 3), activation='relu', input_shape=(image_width,image_width,3)))
+model.add(Conv2D(32, (3, 3), activation='relu'))
+model.add(MaxPooling2D((2, 2)))
+model.add(Dropout(0.25))
+model.add(Conv2D(64, (3, 3), activation='relu'))
+model.add(Conv2D(64, (3, 3), activation='relu'))
+model.add(Conv2D(64, (3, 3), activation='relu'))
+model.add(MaxPooling2D((3, 3)))
+model.add(Dropout(0.25))
+model.add(Conv2D(128, (3, 3), activation='relu'))
+model.add(Conv2D(128, (3, 3), activation='relu'))
+model.add(Conv2D(128, (3, 3), activation='relu'))
+model.add(MaxPooling2D((3, 3)))
+model.add(Dropout(0.25))
+model.add(Conv2D(128, (3, 3), activation='relu'))
+model.add(Conv2D(128, (3, 3), activation='relu'))
+model.add(Conv2D(128, (3, 3), activation='relu'))
+model.add(MaxPooling2D((3, 3)))
+'''
+'''512
+model.add(Conv2D(32, (3, 3), activation='relu'))
+model.add(MaxPooling2D((2, 2)))
+model.add(Dropout(0.25))
+model.add(Conv2D(64, (3, 3), activation='relu'))
+model.add(Conv2D(64, (3, 3), activation='relu'))
+model.add(Conv2D(64, (3, 3), activation='relu'))
+model.add(MaxPooling2D((2, 2)))
+model.add(Conv2D(64, (3, 3), activation='relu'))
+model.add(Conv2D(64, (3, 3), activation='relu'))
+model.add(Conv2D(64, (3, 3), activation='relu'))
+model.add(MaxPooling2D((2, 2)))
+model.add(Dropout(0.25))
+model.add(Conv2D(128, (3, 3), activation='relu'))
+model.add(Conv2D(128, (3, 3), activation='relu'))
+model.add(Conv2D(128, (3, 3), activation='relu'))
+model.add(MaxPooling2D((2, 2)))
+model.add(Conv2D(128, (3, 3), activation='relu'))
+model.add(Conv2D(128, (3, 3), activation='relu'))
+model.add(Conv2D(128, (3, 3), activation='relu'))
+model.add(MaxPooling2D((3, 3)))
+'''
 
 model.add(Flatten())
-#model.add(Dropout(0.5))
+model.add(Dropout(0.5))
 model.add(Dense(512, activation='relu'))
 model.add(Dense(20, activation='softmax'))
 
@@ -141,9 +247,9 @@ train_datagen2 = ImageDataGenerator( featurewise_center=False,  # set input mean
                                     zca_epsilon=1e-06,  # epsilon for ZCA whitening
                                     rotation_range=30,  # randomly rotate images in the range (degrees, 0 to 180)
                                     # randomly shift images horizontally (fraction of total width)
-                                    width_shift_range=0.,
+                                    width_shift_range=0.1,
                                     # randomly shift images vertically (fraction of total height)
-                                    height_shift_range=0.,
+                                    height_shift_range=0.1,
                                     shear_range=0.,  # set range for random shear
                                     zoom_range=0.,  # set range for random zoom
                                     channel_shift_range=0.,  # set range for random channel shifts
@@ -164,10 +270,9 @@ test_datagen = ImageDataGenerator( rescale=1. / 255)
 
 train_generator = train_datagen.flow_from_directory(
     train_dir,
-    color_mode=color_mode,
     target_size=(image_width,image_width),
     batch_size=batch_size,
-    save_to_dir=(tmp_dir+'/train/') if save_to_dir else None,
+    #save_to_dir=tmp_dir+'/train/',
     subset='training',
     shuffle=True)
 random.shuffle(train_generator.filenames)
@@ -181,18 +286,15 @@ train_generator.set_processing_attrs(train_datagen2,
                                      train_generator.save_format,
                                      train_generator.subset,
                                      train_generator.interpolation)
-
 valid_generator = train_datagen.flow_from_directory(
     train_dir, # same directory as training data
-    color_mode=color_mode,
     target_size=(image_width, image_width),
-    save_to_dir=(tmp_dir+'/valid/') if save_to_dir else None,
-    batch_size=batch_size,
+    #save_to_dir=tmp_dir+'/valid/',
+    batch_size=1,
     subset='validation') # set as validation data
 valid_generator.filenames
 test_generator = test_datagen.flow_from_directory(
     test_dir,
-    color_mode=color_mode,
     target_size=(image_width,image_width),
     batch_size=1,
     class_mode=None,  # only data, no labels
@@ -212,7 +314,7 @@ history_o = model.fit_generator(
     epochs = epochs,
     callbacks=callbacks,
     verbose=verbose,
-    workers=4)
+    workers=3)
 
 elapsed = time.time() - t
 
